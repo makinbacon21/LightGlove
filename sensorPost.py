da466df9d846533b2fed8c92815540e86e331fdc
Thomas Makin <halorocker89@gmail.com>
2020-12-16 18:24:17 -0500

    re-add ifttt connection, define threshold, comment

--- sensorPost.py
+++ sensorPost.py
@@ -6,14 +6,28 @@ import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
+
+# define spi bus to MCP3008 and MCP3008 object
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 cs = digitalio.DigitalInOut(board.D5)
 mcp = MCP.MCP3008(spi, cs)
 
+# threshold value (TBD)
+thresh = 6000
+
+# define analog channel
 channel = AnalogIn(mcp, MCP.P0)
 
+# loop--post to ifttt endpoint if voltage > threshold value
 while True:
+    #<DEBUG>
     print('Raw ADC Value: ', channel.value)
     print('ADC Voltage: ' + str(channel.voltage) + 'V')
-    time.sleep(0.5)
-    #requests.post("https://maker.ifttt.com/trigger/rpi_test/with/key/dl19EHfc-UHyIuQ-5AjJEQ"
+    #</DEBUG>
+
+    # if voltage > threshold, post to ifttt, wait to prevent multi activation, else wait 0.5s
+    if channel.voltage > thresh:
+        requests.post("https://maker.ifttt.com/trigger/rpi_test/with/key/dl19EHfc-UHyIuQ-5AjJEQ")
+        time.sleep(3)
+    else:
+        time.sleep(0.5)
