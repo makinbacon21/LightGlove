deddd09469df8cef783ce2f4ca91b676a015dece
Thomas Makin <halorocker89@gmail.com>
2020-12-22 06:40:12 +0000

    set up for dfrobot board--it works now

--- sensorPost.py
+++ sensorPost.py
@@ -1,33 +1,26 @@
 import requests
-
 import time
-import busio
-import digitalio
-import board
-import adafruit_mcp3xxx.mcp3008 as MCP
-from adafruit_mcp3xxx.analog_in import AnalogIn
+from DFRobot_RaspberryPi_Expansion_Board import DFRobot_Expansion_Board_IIC as Board
 
-# define spi bus to MCP3008 and MCP3008 object
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-cs = digitalio.DigitalInOut(board.D5)
-mcp = MCP.MCP3008(spi, cs)
+board = Board(1, 0x10)  # Select i2c bus 1, set address to 0x10
 
 # threshold value (TBD)
-thresh = 6000
+thresh = 1000
 
-# define analog channel
-channel = AnalogIn(mcp, MCP.P0)
+if __name__ == "__main__":
+  while board.begin() != board.STA_OK:  # Board begin and check the board's status
+    print("Board begin failed.")
+    time.sleep(1)
+print("Board begin success.")
+board.set_adc_enable()
 
 # loop--post to ifttt endpoint if voltage > threshold value
 while True:
-    #<DEBUG>
-    print('Raw ADC Value: ', channel.value)
-    print('ADC Voltage: ' + str(channel.voltage) + 'V')
-    #</DEBUG>
 
     # if voltage > threshold, post to ifttt, wait to prevent multi activation, else wait 0.5s
-    if channel.voltage > thresh:
+    if board.get_adc_value(board.A0) < thresh:
+        print("Event triggered")
         requests.post("https://maker.ifttt.com/trigger/rpi_test/with/key/dl19EHfc-UHyIuQ-5AjJEQ")
         time.sleep(3)
     else:
-        time.sleep(0.5)
+        time.sleep(1)
