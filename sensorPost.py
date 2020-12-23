893efc831db617f1de969f7b9fdcc48dae4ed7ed
Thomas Makin <halorocker89@gmail.com>
2020-12-23 09:19:45 -0500

    comments

--- sensorPost.py
+++ sensorPost.py
@@ -9,21 +9,21 @@ thresh = 1300
 
 if __name__ == "__main__":
   while board.begin() != board.STA_OK:  # Board begin and check the board's status
-    print("Board begin failed.")
+    print("Board begin failed.") # debug logging
     time.sleep(1)
-print("Board begin success.")
+print("Board begin success.") # debug logging
 board.set_adc_enable()
 
-# loop--post to ifttt endpoint if voltage > threshold value
+# infinite loop
 while True:
 
-    # if voltage > threshold, post to ifttt, wait to prevent multi activation, else wait 0.5s
+    # if voltage < threshold, post to ifttt, wait to prevent multi activation, else wait 0.5s
     if board.get_adc_value(board.A0) < thresh:
-        print("pointer triggered")
+        print("pointer triggered") # debug logging
         requests.post("https://maker.ifttt.com/trigger/pointer_finger/with/key/esey_sUu1A1kbufOqfcjz4fYppk18XBgfJMeJkhNMoX")
         time.sleep(3)
     elif board.get_adc_value(board.A1) < thresh:
-        print("middle triggered")
+        print("middle triggered") # debug logging
         requests.post("https://maker.ifttt.com/trigger/middle_finger/with/key/esey_sUu1A1kbufOqfcjz4fYppk18XBgfJMeJkhNMoX")
         time.sleep(3)
     else:
